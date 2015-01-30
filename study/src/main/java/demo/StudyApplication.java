d2871241bd7326719157cab7e268be54025584af
leedonil <nanstorng2@nate.com>
2015-01-30 17:34:47 +0900

    스터디2차

--- study/src/main/java/demo/StudyApplication.java
+++ study/src/main/java/demo/StudyApplication.java
@@ -1,9 +1,19 @@
 package demo;
 
+import java.nio.charset.Charset;
+
+import javax.servlet.Filter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+
 
 @SpringBootApplication
 @Configuration
@@ -13,5 +23,16 @@ public class StudyApplication {
     public static void main(String[] args) {
         SpringApplication.run(StudyApplication.class, args);
     }
-    
+    @Bean
+    public HttpMessageConverter<String> responseBodyConverter() {
+        return new StringHttpMessageConverter(Charset.forName("UTF-8"));
+    }
+ 
+    @Bean
+    public Filter characterEncodingFilter() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return characterEncodingFilter;
+    }
 }
--- study/src/main/java/demo/TestController.java
+++ study/src/main/java/demo/TestController.java
@@ -1,22 +1,78 @@
 package demo;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+
+
 @Controller
 public class TestController {
-
-	@RequestMapping("/")
+	
+	@Autowired private UserRepository userRepository;
+	
+	private Logger logger = LoggerFactory.getLogger("logback.test");
+	
+	/*@RequestMapping("/")
 	@ResponseBody
-	public String helloworld() {
-		return "hello world";
+	public User helloworld() {
+		 logger.info(" 로그백 파일로그 테스트");
+		 
+		 return userRepository.(1l);
+	}*/
+	
+	@RequestMapping("/user")
+	@ResponseBody
+	public List<User> helloworld2() {
+		
+		return userRepository.findByAgeAndName("2", "3");
 	}
 	
-	@RequestMapping("/test")
-	public ModelAndView index() {
+	@RequestMapping("/user2")
+	@ResponseBody
+	public List<User> getUser() {
 		
-		return new ModelAndView("test");
+		return userRepository.findAll();
+	}
+	
+	
+	
+	
+	@RequestMapping(value="/index")
+	public ModelAndView index() {
+		ModelAndView modelAndView = new ModelAndView("index");
+		modelAndView.addObject("userName1", "이돈일");
+		modelAndView.addObject("userName2", "이지훈");
+		
+		List<Map<String,String>> messagesList = new ArrayList<Map<String,String>>();
+		Map<String, String> messagesMap = new HashMap<String, String>();
+		messagesMap.put("name", "이돈일");
+		messagesMap.put("email", "test@naver.com");
+		messagesMap.put("phone", "0101234567");
+		messagesMap.put("website", "naver.com");
+		messagesList.add(messagesMap);
+		
+		messagesMap = new HashMap<String, String>();
+		messagesMap.put("name", "이지훈");
+		messagesMap.put("email", "test2@naver.com");
+		messagesMap.put("phone", "0102234567");
+		messagesMap.put("website", "naver2.com");
+		messagesList.add(messagesMap);
+		
+		
+		
+	
+		
+		modelAndView.addObject("messages", messagesList);
+		return modelAndView;
 	}
 }
